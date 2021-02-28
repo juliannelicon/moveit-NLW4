d9c4c21214cc4fd37180b8cf587245e2032a85b8
Julianne <juliannelicon@gmail.com>
2021-02-28 14:49:12 -0300

    Initial commit

--- src/components/LevelUpModal.tsx
+++ src/components/LevelUpModal.tsx
@@ -3,11 +3,11 @@ import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/LevelUpModal.module.css'
 
 export function LevelUpModal() {
-  const { closeLevelUpModal } = useContext(ChallengesContext)
+  const { level, closeLevelUpModal } = useContext(ChallengesContext)
   return (
     <div className={styles.overlay}>
       <div className={styles.container}>
-        <header>2</header>
+        <header>{level}</header>
 
         <strong>Parabéns</strong>
         <p>Você alcançou um novo level.</p>
